35991e096640462d375e9dfbb83649a7ee1a4d65
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2026-05-20 21:41:53 -0300

    Desseleciona uma tarefa

--- Projeto-Fokus/script-crud.js
+++ Projeto-Fokus/script-crud.js
@@ -12,6 +12,7 @@ const paragrafoDescricaoTarefa = document.querySelector(
 );
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefaSelecionada = null;
 
 function atualizarTarefa() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -59,6 +60,17 @@ function criarElementoTarefa(tarefa) {
   li.append(botao);
 
   li.onclick = () => {
+    document
+      .querySelectorAll(".app__section-task-list-item-active")
+      .forEach((elemento) => {
+        elemento.classList.remove("app__section-task-list-item-active");
+      });
+    if (tarefaSelecionada == tarefa) {
+      paragrafoDescricaoTarefa.textContent = "";
+      tarefaSelecionada = null;
+      return;
+    }
+    tarefaSelecionada = tarefa;
     paragrafoDescricaoTarefa.textContent = tarefa.descricao;
     li.classList.add("app__section-task-list-item-active");
   };
